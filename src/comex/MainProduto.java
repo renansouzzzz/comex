7972af856c9bc06c8d871e575b910483931d8c07
Renan <renansouza.oliv@outlook.com>
2022-09-19 17:49:32 -0300

    Classe ProdutoIsento

--- src/comex/MainProduto.java
+++ src/comex/MainProduto.java
@@ -5,6 +5,7 @@ public class MainProduto {
 		Produto produto1 = new Produto();
 		Produto produto2 = new Produto();
 		Produto produto3 = new Produto();
+		ProdutoIsento produto2Isento = new ProdutoIsento();
 	
 		produto1.id = 1;
 		produto1.nome = "Notebook Samsung";
@@ -23,6 +24,13 @@ public class MainProduto {
 		produto3.precoUnitario = 1889.00;
 		produto3.quantidadeEstoque = 3;
 		produto3.categoriaProduto = "Informática";
+		
+		produto2Isento.id = 2;
+		produto2Isento.nome = "Clean Architecture";
+		produto2Isento.precoUnitario = 109.90;
+		produto2Isento.quantidadeEstoque = 2;
+		produto2Isento.categoriaProduto = "Livros";
+		
 		
 		System.out.println("Produto " + produto1.id);
 		System.out.println("ID: " + produto1.id);
@@ -52,6 +60,18 @@ public class MainProduto {
 		System.out.println("Categoria: " + produto3.categoriaProduto);
 		System.out.println("Total de valor incluindo estoque: " + produto3.calculaValorEst());
 		System.out.println("Cálculo de imposto: " + produto3.calculaImposto());
+		
+		
+		// Neste caso faremos o produto 2 utilizar a funcao produto isento
+		System.out.println();
+		System.out.println("Produto " + produto2Isento.id);
+		System.out.println("ID: " + produto2Isento.id);
+		System.out.println("Nome: " + produto2Isento.nome);
+		System.out.println("Preço Unitário: " + produto2Isento.precoUnitario);
+		System.out.println("Quantidade em estoque: " + produto2Isento.quantidadeEstoque);
+		System.out.println("Categoria: " + produto2Isento.categoriaProduto);
+		System.out.println("Total de valor incluindo estoque: " + produto2Isento.calculaValorEst());
+		System.out.println("Cálculo de imposto: " + produto2Isento.calculaImpostoIsento());
 
 	}
 }
---
+++ src/comex/ProdutoIsento.java
@@ -0,0 +1,40 @@
+package comex;
+
+public class ProdutoIsento {
+	int id;
+	String nome;
+	String descricao;
+	double precoUnitario;
+	int quantidadeEstoque;
+	String categoriaProduto;
+
+
+	public int getId() {
+		return this.id;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	public double getPrecoUni() {
+		return this.precoUnitario;
+	}
+	
+	public int getQuantidadeEst() {
+		return this.quantidadeEstoque;
+	}
+	
+	public String getCategoriaProd() {
+		return this.categoriaProduto;
+	}
+	
+	public double calculaValorEst() {
+		return this.quantidadeEstoque * this.precoUnitario;
+	}
+	
+	public double calculaImpostoIsento() {
+		return 0;
+	}
+}
+
