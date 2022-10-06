a882974efbc892b95fe0b1cca131636824c49348
Renan <renansouza.oliv@outlook.com>
2022-10-06 10:28:47 -0300

    Calculando total de categorias em pedidos.csv

---
+++ src/br/com/comex/csv/TotalCategorias.java
@@ -0,0 +1,41 @@
+package br.com.comex.csv;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class TotalCategorias {
+	public static void main(String[] args) throws FileNotFoundException {
+
+		List lista = new ArrayList<PedidoCsv>();
+		FileInputStream file = new FileInputStream("pedidos.csv");
+		Scanner scanner = new Scanner(file);
+				
+		scanner.nextLine();
+		
+		String split[] = null;
+		
+		
+		
+		while (scanner.hasNextLine()) {
+			String linha = scanner.nextLine();
+			split = linha.split(",");
+			if(lista.contains(split[0])) continue;
+				lista.add(split[0]);
+		}
+
+		scanner.close();
+			
+	System.out.println("Total de categorias: " + lista.size() + "  -  " + lista);
+
+	
+	}		
+}
+
+
+	
+
