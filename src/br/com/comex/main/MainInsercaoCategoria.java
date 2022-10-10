6402c60ab0f5fea6f8c698af5f5bfe0df8553391
Renan <renansouza.oliv@outlook.com>
2022-10-10 12:09:42 -0300

    insercao de categorias via jdbc - done

--- src/br/com/comex/main/MainInsercaoCategoria.java
+++ src/br/com/comex/main/MainInsercaoCategoria.java
@@ -1,7 +1,9 @@
 package br.com.comex.main;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainInsercaoCategoria {
 
@@ -10,6 +12,14 @@ public class MainInsercaoCategoria {
 		ConnectionFactory testaCon = new ConnectionFactory();
 		Connection con = testaCon.IniciaConexao();
 		
+		Statement stm = con.createStatement();
+		
+		stm.execute("INSERT INTO comex.CATEGORIA(nome, status) VALUES ('INFORMÁTICA', 'ATIVA')");
+		stm.execute("INSERT INTO comex.CATEGORIA(nome, status) VALUES ('MÓVEIS', 'INATIVA')");
+		stm.execute("INSERT INTO comex.CATEGORIA(nome, status) VALUES ('LIVROS', 'ATIVA')");
+		
+		System.out.println("Operação executada com sucesso!");
+		con.close();
 		
 	}
 
