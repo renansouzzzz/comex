b9b173a7170914a5443f654b31c0c2635ff057d6
Renan <renansouza.oliv@outlook.com>
2022-10-10 13:34:17 -0300

    select categoria - listagem de categorias

---
+++ src/br/com/comex/main/MainListagemCategoria.java
@@ -0,0 +1,29 @@
+package br.com.comex.main;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainListagemCategoria {
+
+	public static void main(String[] args) throws SQLException {
+		ConnectionFactory conectorFac = new ConnectionFactory();
+		Connection con = conectorFac.IniciaConexao();
+		Statement stm = con.createStatement();
+		
+		stm.execute("SELECT * FROM comex.CATEGORIA");
+		ResultSet result = stm.getResultSet();
+		
+		while (result.next()) {
+			Integer id = result.getInt("id");
+			String nome = result.getString("nome");
+			String status = result.getString("status");
+			
+			System.out.println("Id: " + id + "\n"
+							+ "Nome: " + nome + "\n"
+							+ "Status: " + status + "\n");
+		}
+	}
+
+}
