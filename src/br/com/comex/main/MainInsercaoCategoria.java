0a5b879e6ef1dd21268f8ec2324c269ddb6fd7a7
Renan <renansouza.oliv@outlook.com>
2022-10-10 11:00:08 -0300

    integracao a jdbc - connection factory done

--- src/br/com/comex/main/MainInsercaoCategoria.java
+++ src/br/com/comex/main/MainInsercaoCategoria.java
@@ -1,10 +1,16 @@
 package br.com.comex.main;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class MainInsercaoCategoria {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory testaCon = new ConnectionFactory();
+		Connection con = testaCon.IniciaConexao();
+		
+		
 	}
 
 }
