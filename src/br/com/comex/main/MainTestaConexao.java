67f31b48b2a23e730135fd98193db3ba5d015b0f
Renan <renansouza.oliv@outlook.com>
2022-10-10 10:53:48 -0300

    integracao a jdbc - connection factory done

--- src/br/com/comex/main/MainTestaConexao.java
+++ src/br/com/comex/main/MainTestaConexao.java
@@ -5,15 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MainTestaConexao {
-	public static void main(String[] args) throws SQLException {
-		
-		Connection con = DriverManager
-				.getConnection("jdbc:oracle:thin:@localhost:1522:xe", "system", "admin");
-		System.out.println("Conexão iniciada com êxito!");
-		
-		
-		con.close();
-		System.out.println("Conexão finalizada com êxito!");
-	}
 	
+		public Connection TestaConexao() throws SQLException{
+			
+		return DriverManager
+				.getConnection("jdbc:oracle:thin:@localhost:1522:xe", "system", "admin");
+		}	
 }
