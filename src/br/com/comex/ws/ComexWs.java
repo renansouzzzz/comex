86726ef7f1082ff7ebfb93330e613223faacfcc9
Renan <renansouza.oliv@outlook.com>
2022-10-20 13:50:13 -0300

    listar produtos soap ui - ws

--- src/br/com/comex/ws/ComexWs.java
+++ src/br/com/comex/ws/ComexWs.java
@@ -2,7 +2,6 @@ package br.com.comex.ws;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.jws.WebMethod;
@@ -11,9 +10,11 @@ import javax.jws.WebService;
 
 import br.com.comex.dao.jdbc.CategoriaDAO;
 import br.com.comex.dao.jdbc.ClienteDAO;
+import br.com.comex.dao.jdbc.ProdutoDAO;
 import br.com.comex.models.Categoria;
 import br.com.comex.models.Cliente;
 import br.com.comex.models.ConnectionFactory;
+import br.com.comex.models.Produto;
 
 @WebService
 public class ComexWs {
@@ -23,6 +24,7 @@ public class ComexWs {
 	
 	private CategoriaDAO categoria = new CategoriaDAO(connection);
 	private ClienteDAO cliente = new ClienteDAO(connection);
+	private ProdutoDAO produto = new ProdutoDAO(connection);
 
 	public List<Categoria> getCategorias() throws SQLException {
 		List<Categoria> listaC = categoria.listagemCategoria();
@@ -53,6 +55,13 @@ public class ComexWs {
 		cliente.insereCliente(c);
 		
 		return c;
+	}
+	
+	@WebMethod(operationName="ListarProdutos")
+	public List<Produto> listarProdutos() throws SQLException {
+		List<Produto> listaP = produto.listagemProduto();
+		
+		return listaP;
 		
 	}
 }
